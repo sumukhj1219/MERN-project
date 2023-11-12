634e0205608adb4af4dc430eb86fd3396367a3bd
Sumukh <sumukhjoshi75@gmail.com>
2023-11-12 19:01:46 +0530

    generated access tokens and refresh tokens

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -46,4 +46,46 @@ const useSchema = new Schema(
 	}
 	)
 
+    useSchema.pre("save", async function(next){
+		if(!this.isModified("password")) return next();
+		this.password = bcrypt.hash(this.password, 10);
+		next()
+	})
+
+	useSchema.methods.isPasswordCorrect = async function(password){
+        return await bcrypt.compare(password, this.password);
+	}
+
+	userSchema.methods.generateAccessToken = function(){
+		return jwt.sign(
+			{
+				_id: this._id,
+				email: this.email,
+				username: this.username,
+				fullname: this.fullname
+			},
+			process.env.ACCESS_TOKEN_SECRET,
+			{
+				expiresIn: ACCESS_TOKEN_EXPIRY
+			}
+		)
+	}
+
+	userSchema.methods.generateAccessToken = function(){
+		return jwt.sign(
+			{
+				_id: this._id,
+			},
+			process.env.REFRESH_TOKEN_SECRET,
+			{
+				expiresIn: REFRESH_TOKEN_EXPIRY
+			}
+		)
+	}
+
+
+	userSchema.methods.generateRefreshToken = function(){}
+
+
+
 export const User = mongoose.model("User", userSchema);
